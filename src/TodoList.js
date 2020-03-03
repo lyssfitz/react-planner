abca93f197699ca97c6e36e18961168b20042b3f
Lyss <lyssfitz@outlook.com>
2020-03-03 14:47:04 +1100

    Add Todo component and render in TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
 
 class TodoList extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { todos: [] };
+		this.state = { todos: [{ task: '30 minute workout' }, { task: 'Make React app' }] };
 	}
 	render() {
+		const todos = this.state.todos.map(todo => {
+			return <Todo task={todo.task} />;
+		});
 		return (
 			<div>
 				<h1>Todo List!</h1>
-				<ul>
-					<li>Todo 1</li>
-					<li>Todo 1</li>
-				</ul>
+				<ul>{todos}</ul>
 			</div>
 		);
 	}
