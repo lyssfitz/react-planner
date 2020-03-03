4d6ab26446f58b96de57dfaf76833d27d719b1a1
Lyss <lyssfitz@outlook.com>
2020-03-03 15:06:20 +1100

    Add NewTodoForm component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
 
 class TodoList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { todos: [{ task: '30 minute workout' }, { task: 'Make React app' }] };
+		this.create = this.create.bind(this);
+	}
+	create(newTodo) {
+		this.setState({
+			todos: [...this.state.todos, newTodo],
+		});
 	}
 	render() {
 		const todos = this.state.todos.map(todo => {
@@ -13,6 +20,7 @@ class TodoList extends Component {
 		return (
 			<div>
 				<h1>Todo List!</h1>
+				<NewTodoForm createTodo={this.create} />
 				<ul>{todos}</ul>
 			</div>
 		);
